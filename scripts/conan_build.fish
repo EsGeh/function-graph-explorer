f5e8b05a10cdf85c6d26380a861cecced07473b8
EsGeh <samuel@esgeh.net>
2024-04-07 14:44:41 +0200

    assure failure propagation in build scripts

--- scripts/conan_build.fish
+++ scripts/conan_build.fish
@@ -8,12 +8,12 @@ set BUILD_DIR $BASE_DIR/build/conan/$BUILD_TYPE
 # configure
 conan install $BASE_DIR --output-folder "$BUILD_DIR" --build missing --settings build_type=$BUILD_TYPE
 
-cmake -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+and cmake -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
 # build
-cmake --build $BUILD_DIR $argv
+and cmake --build $BUILD_DIR $argv
 
 # create './compile_commands.json'
 # which helps editors and IDEs
 # finding source files:
-cmake --build $BUILD_DIR --target CopyCompileCommands
+and cmake --build $BUILD_DIR --target CopyCompileCommands
